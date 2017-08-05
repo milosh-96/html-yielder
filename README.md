0bda0e317c9113f2f076ce7d86e91d215f3b13c4
milosh-96 <milosjovanovic042@gmail.com>
2017-08-05 22:05:49 +0200

    Read me

---
+++ README.md
@@ -0,0 +1,5 @@
+# HTML Yielder
+## Miloš Jovanović
+
+*** 
+
---
+++ inc/variables.php
@@ -0,0 +1,11 @@
+<?php
+    // define your variables here
+    // into square brackets place your variables
+    // format: "name"=>"HTMLYielder"
+    // Remember to put ',' at the end of key/value pair
+    // After the last pair, ',' is not required!
+    // Bellow is an example with two variables.
+    // You will insert {name} and {version} in your HTML, and then values "HTMLYielder" and "0.1" will appear.
+    // Visit our web page http://www.html-yielder.info for more information and support.
+
+    $variables = ["name"=>"HTMLYielder","version"=>"0.1"];
